f9de5f6443a73f8bdc0d644ebac59e8bd66cb306
edriso <edriso.dev@gmail.com>
2023-09-18 23:58:01 +0300

    add basic structure

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('E Commerce API');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const startServer = async () => {
+  try {
+    const port = process.env.PORT || 5000;
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log('Error while starting the server 💥', error);
+  }
+};
+
+startServer();
