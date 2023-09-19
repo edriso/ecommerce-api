2f295ecd9c66126d638eac1581ec2402c60385ce
edriso <edriso.dev@gmail.com>
2023-09-19 10:35:57 +0300

    create user model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      validate: {
+        validator: validator.isEmail,
+        message: 'Invalid email',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps,
+  },
+);
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = user;
