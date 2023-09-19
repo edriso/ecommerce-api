dc3c5932486f8f80337b7d5c7796e94e40e7cb54
edriso <edriso.dev@gmail.com>
2023-09-19 11:36:31 +0300

    implement register endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,20 @@
 const User = require('../models/user');
+const CustomError = require('../errors');
 
 const register = async (req, res) => {
-  res.status(201).send('register');
+  const { name, email, password } = req.body;
+
+  const emailExists = await User.find({ email });
+  if (emailExists) {
+    throw new CustomError.BadRequestError('Email already in use');
+  }
+
+  //   // Register first user as an admin
+  //   const isFirstAccount = (await User.countDocuments({})) === 0;
+  //   const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password });
+  res.status(201).json({ user });
 };
 
 const login = async (req, res) => {
