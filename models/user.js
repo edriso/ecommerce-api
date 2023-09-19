176120832e769e0a11741abf59f64a150b009eb1
edriso <edriso.dev@gmail.com>
2023-09-19 10:51:03 +0300

    update variable name

--- models/user.js
+++ models/user.js
@@ -33,6 +33,6 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports = user;
+module.exports = User;
