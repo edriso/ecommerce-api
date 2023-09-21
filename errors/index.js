5a39084118392cf08201bd218a3e4d36c5f27a89
edriso <edriso.dev@gmail.com>
2023-09-21 14:56:43 +0300

    add forbidden error

--- errors/index.js
+++ errors/index.js
@@ -2,9 +2,11 @@ const CustomAPIError = require('./custom-api');
 const UnauthenticatedError = require('./unauthenticated');
 const NotFoundError = require('./not-found');
 const BadRequestError = require('./bad-request');
+const ForbiddenError = require('./forbidden');
 module.exports = {
   CustomAPIError,
   UnauthenticatedError,
   NotFoundError,
   BadRequestError,
+  ForbiddenError,
 };
