cbba8d5a90bae78027e92a20b3e93781a809a689
edriso <edriso.dev@gmail.com>
2023-09-23 00:20:18 +0300

    add product actions

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,6 +5,31 @@ const getAllProducts = async (req, res) => {
   res.send('getAllProducts');
 };
 
+const getSingleProduct = async (req, res) => {
+  res.send('getSingleProduct');
+};
+
+const createProduct = async (req, res) => {
+  res.status(201).send('createProduct');
+};
+
+const updateProduct = async (req, res) => {
+  res.send('updateProduct');
+};
+
+const deleteProduct = async (req, res) => {
+  res.status(204).send('deleteProduct');
+};
+
+const uploadImage = async (req, res) => {
+  res.send('uploadImage');
+};
+
 module.exports = {
   getAllProducts,
+  getSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts } = require('../controllers/productController');
+const {
+  getAllProducts,
+  getSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+} = require('../controllers/productController');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
 
-router.route('/').get(getAllProducts);
+router
+  .route('/')
+  .get(getAllProducts)
+  .post([authenticateUser, authorizePermissions('admin')], createProduct);
+
+router
+  .route('/uploadImage')
+  .post([authenticateUser, authorizePermissions('admin')], uploadImage);
+
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+  .delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
 
 module.exports = router;
