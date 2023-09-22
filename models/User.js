9c17bedb5dbdb9666149d6dbf20308cbc57e471f
edriso <edriso.dev@gmail.com>
2023-09-22 19:07:08 +0300

    on save return if not modifying the password

--- models/User.js
+++ models/User.js
@@ -35,6 +35,7 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
