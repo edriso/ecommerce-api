43bdd32945441c5587a2d1d4d4c98665b6ba3453
edriso <edriso.dev@gmail.com>
2023-09-26 15:54:57 +0300

    fix reviews routes

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/products', productRouter);
-app.use('api/v1/reviews', reviewRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
