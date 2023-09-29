bf12af03bab1aef4b31513d4d507a0d105963c2f
edriso <edriso.dev@gmail.com>
2023-09-29 11:05:12 +0300

    move authenticateUser middleware to userRoutes instead of app.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,12 +7,24 @@ const {
   updateUser,
   updateUserPassword,
 } = require('../controllers/userController');
-const { authorizePermissions } = require('../middleware/authentication');
+const {
+  authorizePermissions,
+  authenticateUser,
+} = require('../middleware/authentication');
 
-router.route('/').get(authorizePermissions('admin'), getAllUsers);
-router.route('/showMe').get(showCurrentUser);
-router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
-router.route('/:id').get(getSingleUser);
+/**
+ * authenticateUser middleware is used in all routes, so we could do it like this in app.js:
+ * app.use('/api/v1/users', authenticateUser, userRouter);
+ * but I liked this setup as I can clearly see what middlewares I'm using, and to be aware of which is following which
+ * eg. authorizePermissions should be following authenticateUser
+ */
+
+router
+  .route('/')
+  .get([authenticateUser, authorizePermissions('admin')], getAllUsers);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
+router.route('/updateUser').patch(authenticateUser, updateUser);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;
