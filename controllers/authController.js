3f54bd0086ff1806578b52fdbd05ada2fbcfe1fa
edriso <edriso.dev@gmail.com>
2023-09-30 21:18:05 +0300

    fix module not found error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
